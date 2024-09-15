6fdd9edcc7f0efa38b9ebba2b96a7337c28208cc
Moneer <moneer.alromimah@gmail.com>
2024-09-15 01:03:41 -0600

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/dashboard/layout/layout.component';
+import { OverviewComponent } from './pages/overview/overview.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: 'overview', component: OverviewComponent }],
+  },
 ];
