6c5cf7f2458dd42f5fabb42ccbc359a61dbf5acc
Moneer <moneer.alromimah@gmail.com>
2024-09-19 14:56:33 -0600

    add patient edit and ne patient components

---
+++ src/app/pages/patients/patient/patient.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { PatientTableComponent } from '../../../components/patients/patient-table/patient-table.component';
+import {
+  IpatientInvoices,
+  PatientService,
+} from '../../../services/patient.service';
+
+@Component({
+  selector: 'app-patient',
+  standalone: true,
+  imports: [PatientTableComponent],
+  templateUrl: './patient.component.html',
+  styleUrl: './patient.component.scss',
+})
+export class PatientComponent implements OnInit {
+  id = input.required<string>();
+  patientService = inject(PatientService);
+  patientInvoices = signal<IpatientInvoices[]>([]);
+
+  ngOnInit() {
+    this.loadInvoices(this.id());
+  }
+
+  loadInvoices(id: string) {
+    this.patientService.getPatientInvoices(id).subscribe({
+      next: (invoices) => {
+        this.patientInvoices.set(invoices);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+}
